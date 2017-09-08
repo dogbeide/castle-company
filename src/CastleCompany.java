f3f9b564e32bfcf6c6ff3f2b101d1a439bfa41ec
David Ogbeide <boyowao@gmail.com>
2017-09-07 18:37:11 -0600

    skeleton code

---
+++ src/CastleCompany.java
@@ -0,0 +1,21 @@
+
+public class CastleCompany {
+	
+	public static void main(String[] args){
+		int length, castles;
+		int [] land = new int[1];
+		
+		//TODO: get user input of land, (or input from file)
+		
+		castles = getCastleCount(land);
+		System.out.println(castles);
+	}
+	
+	public static int getCastleCount(int[] land){
+		
+		//TODO: order(n) speed index-stepping algorithm
+		
+		return 0; //Default, method stub
+	}
+
+}
